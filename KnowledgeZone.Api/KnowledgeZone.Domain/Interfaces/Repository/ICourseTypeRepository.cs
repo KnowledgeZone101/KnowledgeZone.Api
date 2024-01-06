ab64934c7c759d7eed8ceca9f43483b2a83abb3e
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-01-06 17:32:29 +0500

    Create Interfaces

---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Interfaces/Repository/ICourseTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿using KnowledgeZone.Domain.Entities;
+
+namespace KnowledgeZone.Domain.Interfaces.Repository
+{
+    public interface ICourseTypeRepository : IRepositoryBase<CourseType>
+    {
+    }
+}
---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Interfaces/Repository/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿using KnowledgeZone.Domain.Entities;
+
+namespace KnowledgeZone.Domain.Interfaces.Repository
+{
+    public interface IStudentRepository : IRepositoryBase<Student>
+    {
+    }
+}
