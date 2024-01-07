3ac0ed87e76083169304627f46be89d0acf7630d
MShArAFidDiNM <129146459+SHaRAFiDdInM@users.noreply.github.com>
2024-01-07 12:16:53 +0500

    Create-Pagination

---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Pagination/PaginatedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeZone.Domain.Pagination
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool PreviosPage => CurrentPage > 1;
+        public bool NextPage => CurrentPage < TotalPage;
+        public PaginatedList(List<T> list, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+
+            AddRange(list);
+        }
+    }
+}
