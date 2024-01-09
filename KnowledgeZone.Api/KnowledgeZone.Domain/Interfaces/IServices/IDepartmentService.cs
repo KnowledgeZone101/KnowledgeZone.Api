5744c8d51c2ebaf11ee85dd2a4538f7b902dd39b
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-01-09 22:43:30 +0500

    Create IServices

---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Interfaces/IServices/IDepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using KnowledgeZone.Domain.DTOs.Department;
+using KnowledgeZone.Domain.Pagination;
+using KnowledgeZone.Domain.ResourceParameters;
+
+namespace KnowledgeZone.Domain.Interfaces.IServices
+{
+    public interface IDepartmentService
+    {
+        PaginatedList<DepartmentDto> GetDepartment(DepartmentResourceParamentrs departmentParameters);
+        DepartmentDto? GetDepartmentById(int id);
+        DepartmentDto CreateDepartment(DepartmentForCreateDto departmentToCreate);
+        void UpdateDepartment(DepartmentForUpdateDto departmentToUpdate);
+        void DeleteDepartment(int id);
+    }
+}
---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Interfaces/IServices/ILessonService.cs
@@ -0,0 +1,15 @@
+﻿using KnowledgeZone.Domain.DTOs.Lesson;
+using KnowledgeZone.Domain.Pagination;
+using KnowledgeZone.Domain.ResourceParameters;
+
+namespace KnowledgeZone.Domain.Interfaces.IServices
+{
+    public interface ILessonService
+    {
+        PaginatedList<LessonDto> GetLesson(LessonResourceParamentrs lessonParameters);
+        LessonDto? GetLessonById(int id);
+        LessonDto CreateLesson(LessonForCreateDto lessonToCreate);
+        void UpdateLesson(LessonForUpdateDto lessonToUpdate);
+        void DeleteLesson(int id);
+    }
+}
