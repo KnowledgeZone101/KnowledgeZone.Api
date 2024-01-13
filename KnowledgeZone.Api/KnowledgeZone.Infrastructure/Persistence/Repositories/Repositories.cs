f835fbb3092556f8715f7dabeb497ec96aba81bd
MShArAFidDiNM <129146459+SHaRAFiDdInM@users.noreply.github.com>
2024-01-13 16:38:51 +0500

    Created

---
+++ KnowledgeZone.Api/KnowledgeZone.Infrastructure/Persistence/Repositories/Repositories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeZone.Infrastructure.Persistence.NewFolder
+{
+    internal class Repositories
+    {
+    }
+}
