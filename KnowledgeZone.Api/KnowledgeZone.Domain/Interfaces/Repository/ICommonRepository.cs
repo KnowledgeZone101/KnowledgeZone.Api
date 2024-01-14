fb98ce8ff79b787313a7d06cf6f8b6412ad59aeb
MShArAFidDiNM <129146459+SHaRAFiDdInM@users.noreply.github.com>
2024-01-14 11:55:53 +0500

    Create ICommonRepository

---
+++ KnowledgeZone.Api/KnowledgeZone.Domain/Interfaces/Repository/ICommonRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeZone.Domain.Interfaces.Repository
+{
+    public class ICommonRepository
+    {
+        public IAttendanceRepository Attendence { get;}
+        public ICourseRepository Course { get;}
+        public ICourseTypeRepository CourseType { get; }
+        public IDepartmentRepository Department { get; }    
+        public IEnrollmentRepository Enrollment { get; }
+        public ILessonRepository Lesson { get; }
+        public IQualificationRepository Qualification { get; }  
+        public IRoomRepository Room { get; }
+        public IStudentRepository Student { get; }  
+        public ITeacherRepository Teacher { get; }
+    }
+}
