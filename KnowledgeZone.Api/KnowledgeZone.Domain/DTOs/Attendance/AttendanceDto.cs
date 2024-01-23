f09b9e53aa99b88ef6d94d57b60b448243cce5b1
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-01-24 00:04:58 +0500

    Updated

--- KnowledgeZone.Api/KnowledgeZone.Domain/DTOs/Attendance/AttendanceDto.cs
+++ KnowledgeZone.Api/KnowledgeZone.Domain/DTOs/Attendance/AttendanceDto.cs
@@ -3,6 +3,12 @@ using KnowledgeZone.Domain.Entities;
 
 namespace KnowledgeZone.Domain.DTOs.Attendance
 {
-    public record AttendanceDto(int AttendanceId, int StudentId, AttendanceType AttendanceType, Mark? Mark, int LessonId, LessonDto Lesson);
+    public record AttendanceDto(
+        int AttendanceId,
+        int StudentId, 
+        AttendanceType AttendanceType,
+        Mark? Mark, 
+        int LessonId, 
+        LessonDto Lesson);
 
 }
